e2b8cde583438a0b3a021c2c9beb9c9b6c0b52e2
emelozturk <emelozturk@live.com>
2021-05-05 23:06:15 +0300

    Add unittest for prime numbers
    Write unittests for `isprime` and `getprimes` functions. `isprime`
    tests cover negative numbers and 2 as edge cases. `getprimes` tests
    cover empty list returns and a list with a single element as well as
    the normal range tests.

---
+++ test_prime.py
@@ -0,0 +1,21 @@
+import unittest
+import prime
+
+class TestPrime(unittest.TestCase):
+
+    def test_isprime(self):
+        self.assertFalse(prime.isprime(8))
+        self.assertTrue(prime.isprime(2))
+        self.assertFalse(prime.isprime(9))
+        self.assertTrue(prime.isprime(17))
+        self.assertFalse(prime.isprime(-1))
+        self.assertFalse(prime.isprime(-8))
+
+    def test_getprimes(self):
+        self.assertEqual(prime.getprimes(-10, 10), [2, 3, 5, 7])
+        self.assertEqual(prime.getprimes(-10, 1), [])
+        self.assertEqual(prime.getprimes(-10, 2), [2])
+        self.assertEqual(prime.getprimes(100, 103), [101, 103])
+
+if __name__ == '__main__':
+    unittest.main()
